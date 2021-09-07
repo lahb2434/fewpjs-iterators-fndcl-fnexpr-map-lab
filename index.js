c7546a2255fe76f277046d4f86bdc4c2b1083154
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-09-06 21:11:40 -0400

    Done.

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ const tutorials = [
 
 const titleCased = () => {
   // use the .map method on the tutorials to return a new array
-  return tutorials
+  return tutorials.map( tutorial => tutorial.split(' ').map( x => x.charAt(0).toUpperCase() + x.slice(1)).join(' '))
 }
